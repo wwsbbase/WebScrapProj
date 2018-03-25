957360ff25b5bdee38c1607acc70dab4d015e495
D.J. Zou <bopy2008@163.com>
2018-03-25 16:23:11 +0800

    add exception handle logic and correct pic name

--- main.py
+++ main.py
@@ -4,22 +4,30 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from urllib.request import urlretrieve
+from urllib.error import HTTPError, URLError
 
 import re
 
-
 def getBingPicLinks():
     htmlUrl = "https://bing.ioliu.cn"
-    html = urlopen(htmlUrl)
-    # print(html.read();
-    bsObj = BeautifulSoup(html.read(), 'html.parser')
-    # print(bsObj.prettify())
+    try:
+        html = urlopen(htmlUrl)
+    except (HTTPError, URLError) as e:
+        return None
+    
+    try:
+        # print(html.read();
+        bsObj = BeautifulSoup(html.read(), 'html.parser')
+        # print(bsObj.prettify())
+        links = []
+        for link in bsObj.find_all(href=re.compile("force=download")):
+            downloadLink = htmlUrl + link.get('href')
+            print(downloadLink)
+            links.append(downloadLink)
 
-    links = []
-    for link in bsObj.find_all(href=re.compile("force=download")):
-        downloadLink = htmlUrl + link.get('href')
-        print(downloadLink)
-        links.append(downloadLink)
+    except AttributeError as e:
+        return None
+
     return links
 
 
@@ -45,7 +53,6 @@ def main():
     downloadLinks = getBingPicLinks()
     download_pics(downloadLinks)
     print("main done")
-
     
 if __name__ == '__main__':
     main()
