8754943ebb35c6a8982b09f1008378fee9ed6e5b
D.J. Zou <bopy2008@163.com>
2018-03-25 16:11:14 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from urllib.request import urlretrieve
+
+import re
+
+
+def getBingPicLinks():
+    htmlUrl = "https://bing.ioliu.cn"
+    html = urlopen(htmlUrl)
+    # print(html.read();
+    bsObj = BeautifulSoup(html.read(), 'html.parser')
+    # print(bsObj.prettify())
+
+    links = []
+    for link in bsObj.find_all(href=re.compile("force=download")):
+        downloadLink = htmlUrl + link.get('href')
+        print(downloadLink)
+        links.append(downloadLink)
+    return links
+
+
+def download_pics(pic_links):
+    for link in pic_links:
+        localPath = "E:/bingPics/"
+        file_name = link.split('/')[-1]
+
+        file_name = file_name.split('?')[0]
+        
+        print("Downloading file:%s" % file_name)
+        localPath = localPath + file_name + ".jpg"
+        urlretrieve(link, localPath)
+        print("%s downloaded!\n" % file_name)
+
+    print("All Pics downloaded!")
+
+    return
+
+def main():
+    """docstring for main"""
+    # createUI()
+    downloadLinks = getBingPicLinks()
+    download_pics(downloadLinks)
+    print("main done")
+
+    
+if __name__ == '__main__':
+    main()
