a41dde9cbe98c4a022695f2cd45dae96d814a366
D.J. Zou <bopy2008@163.com>
2018-04-22 23:17:29 +0800

    OCR 相关

---
+++ OCR.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import re
+import os
+import base64
+import sys
+
+import subprocess
+from PIL import Image
+import pytesseract
+
+
+def parsePicData():
+    words = pytesseract.image_to_string(Image.open("D:\\Project\\GitHub\\WebScrapProj\\cache.png"), lang='eng')
+    print(words)
+    print("parsePicData")
+
+def testImage():
+    im= Image.open("D:\\Project\\GitHub\\WebScrapProj\\cache.png")
+    print(im)
+    print(im.format,im.size,im.mode)
+
+
+def main():
+    """docstring for main"""
+    print("main start")
+    # createUI()
+    parsePicData()
+
+    print("main done")
+    
+if __name__ == '__main__':
+    main()
